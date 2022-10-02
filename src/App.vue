4b7f365a504e5b5327ca411b9153e113a6210a33
unknown <juanantonio.10.11@gmail.com>
2022-10-02 18:47:19 -0500

    JASM

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+
+  <headerr></headerr>
+
+  <div class="container mt-3">
+   
+
+    <formulario></formulario>
+
+
+
+
+
+
+</div>
+</template>
+
+
+<script>
+  import headerr from "./components/headerr.vue"
+  import formulario from "./components/formulario.vue";
+
+  export default{
+    components: {
+      headerr,
+      formulario,
+    }
+
+  }
+
+</script>
+
+
+<style scoped>
+  div{
+    margin: 0px 2rem;
+  }
+
+</style>
---
+++ src/components/headerr.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+    data: () => ({
+    url:"https://cdn.svgporn.com/logos/vue.svg",
+    alt: "vue.js",
+    
+    }),
+  };
+  
+</script>
+
+
+
+
+<template>
+  <div class = "container mt-3">
+      <div class="row">
+        <div class="col-12 col-md-3">
+          <h1>Formulario</h1>
+        </div>
+        <div class="col-12 col-md-6"> </div>
+            <img :src = "url" :alt="alt" width="100" height="100">
+      </div>
+    </div>
+    
+    <hr />
+
+</template>
+
