ec72e5a25cd27264be76ea58c1f98e433c1ff40e
unknown <juanantonio.10.11@gmail.com>
2022-10-12 09:43:03 -0500

    barra de progreso

---
+++ src/components/ProgresBar.vue
@@ -0,0 +1,22 @@
+<template>
+
+<h3 class="text-center" > Proceso: {{porcentaje}}%  </h3>
+      <div class="progress">
+      <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" 
+            role="progressbar" 
+            v-bind:aria-valuenow="porcentaje" 
+            aria-valuemin="0" 
+            aria-valuemax="100"
+            :style="`width:${porcentaje}%`" 
+            /> 
+      </div>
+
+</template>
+
+<script>
+      export default {
+            props: {
+                  porcentaje: Number,
+            },
+      }
+</script>
